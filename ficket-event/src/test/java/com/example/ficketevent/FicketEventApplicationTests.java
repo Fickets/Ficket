5333d7075d74524205866ab8681fddabfed76231
ohy1023 <zvyg1023@naver.com>
2024-11-11 20:36:23 +0900

    [Feat] Event-Service 구축 #SCRUM-69 #DONE

---
+++ ficket-event/src/test/java/com/example/ficketevent/FicketEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ficketevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicketEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
