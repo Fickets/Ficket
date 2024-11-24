97c752f0777e6d54c581c2812cade3334eeea645
ohy1023 <vyg1023@gmail.com>
2024-11-24 20:30:11 +0900

    [Feat/config] Flask 설정 정보 복호화 적용

--- ficket-face/config.py
+++ ficket-face/config.py
@@ -1,10 +1,22 @@
 import requests
+from jks_utils import load_private_key, decrypt_value
+
 
 # 초기 설정 로드 함수
 def load_config_from_server():
     # Config Server URL 설정
     CONFIG_SERVER_URL = "http://localhost:8888/face-service/local"
+
+    PRIVATE_KEY_PATH = "private.pem"
+    
+
     try:
+        private_key = load_private_key(PRIVATE_KEY_PATH)
+
+        if private_key is None:
+            raise ValueError("Private key could not be loaded")
+        
+
         response = requests.get(CONFIG_SERVER_URL)
         response.raise_for_status()
         config_data = response.json()
@@ -19,11 +31,11 @@ def load_config_from_server():
             if "flask.mysql.url" in source_data:
                 config["mysql"]["url"] = source_data["flask.mysql.url"]
             if "flask.mysql.password" in source_data:
-                config["mysql"]["password"] = source_data["flask.mysql.password"]
+                config["mysql"]["password"] = decrypt_value(source_data["flask.mysql.password"],private_key)
                 
             # Encryption Key 설정
             if "encryption.secret_key" in source_data:
-                config["encryption"]["secret_key"] = source_data["encryption.secret_key"]
+                config["encryption"]["secret_key"] = decrypt_value(source_data["encryption.secret_key"],private_key)
 
             # RabbitMQ 설정
             if "spring.rabbitmq.host" in source_data:
@@ -37,15 +49,14 @@ def load_config_from_server():
             
             # S3 설정
             if "s3.aws.accesskey" in source_data:
-                config["aws"]["accesskey"] = source_data["s3.aws.accesskey"]
+                config["aws"]["accesskey"] = decrypt_value(source_data["s3.aws.accesskey"],private_key)
             if "s3.aws.secretkey" in source_data:
-                config["aws"]["secretkey"] = source_data["s3.aws.secretkey"]
+                config["aws"]["secretkey"] = decrypt_value(source_data["s3.aws.secretkey"],private_key)
             if "s3.aws.bucketname" in source_data:
                 config["aws"]["bucketname"] = source_data["s3.aws.bucketname"]
             if "s3.aws.region" in source_data:
                 config["aws"]["region"] = source_data["s3.aws.region"]
 
-        print("Configuration reloaded:", config)
         return config
     except requests.RequestException as e:
         print("Failed to load configuration from Config Server:", e)
--- ficket-face/s3_utils.py
+++ ficket-face/s3_utils.py
@@ -5,6 +5,15 @@ def upload_file_to_s3(file, folder="faces"):
     """S3에 파일 업로드"""
     file_extension = file.filename.split('.')[-1]
     unique_filename = f"{folder}/{uuid.uuid4().hex}.{file_extension}"
+
+    # SSE-C 설정이 필요할 경우 아래 주석 부분을 활성화하세요
+    # 현재는 HTTPS 설정이 되어 있지 않아 테스트가 불가능합니다.
+    # extra_args = {
+    #     "ContentType": file.content_type,
+    #     "SSECustomerAlgorithm": "AES256",
+    #     "SSECustomerKey": "<your-base64-encoded-key>",
+    #     "SSECustomerKeyMD5": "<your-base64-encoded-md5>"
+    # }
     
     s3.upload_fileobj(
         file,
