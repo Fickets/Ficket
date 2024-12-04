16998c64afdadb449b5f7fbacd81f7efde88d09d
choiyongsu <cys980220@gmail.com>
2024-12-04 11:36:23 +0900

    [feat] user redirect update

--- ficket-user/src/main/java/com/example/ficketuser/global/jwt/CustomSuccessHandler.java
+++ ficket-user/src/main/java/com/example/ficketuser/global/jwt/CustomSuccessHandler.java
@@ -1,7 +1,11 @@
 package com.example.ficketuser.global.jwt;
 
+import com.example.ficketuser.Entity.User;
 import com.example.ficketuser.Entity.UserTokenRedis;
 import com.example.ficketuser.dto.resquest.CustomOAuth2User;
+import com.example.ficketuser.global.result.error.ErrorCode;
+import com.example.ficketuser.global.result.error.exception.BusinessException;
+import com.example.ficketuser.repository.UserRepository;
 import com.example.ficketuser.repository.UserTokenRedisRepository;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,17 +25,20 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
     private final JwtUtils jwtUtils;
     private final UserTokenRedisRepository userTokenRedisRepository;
+    private final UserRepository userRepository;
+
 
     private String REFRESH_HEADER;
     private String ACCESS_HEADER;
     private int REFRESH_TOKEN_MAX_AGE;
 
-    public CustomSuccessHandler(JwtUtils jwtUtils, UserTokenRedisRepository userTokenRedisRepository,
+    public CustomSuccessHandler(JwtUtils jwtUtils, UserTokenRedisRepository userTokenRedisRepository, UserRepository userRepository,
                                 @Value("${jwt.refresh.header}") String refreshHeader,
                                 @Value("${jwt.access.header}") String accessHeader,
                                 @Value("${refresh-token-maxage}") int refreshTokenMaxAge) {
         this.jwtUtils = jwtUtils;
         this.userTokenRedisRepository = userTokenRedisRepository;
+        this.userRepository = userRepository;
         this.REFRESH_HEADER = refreshHeader;
         this.ACCESS_HEADER = accessHeader;
         this.REFRESH_TOKEN_MAX_AGE = refreshTokenMaxAge;
@@ -60,8 +67,14 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
                 .refreshToken(refresh)
                 .build();
         userTokenRedisRepository.save(userTokenRedis);
+        User user = userRepository.findByUserId(userId)
+                        .orElseThrow(() -> new BusinessException(ErrorCode.NOT_USER_FOUND));
+        if (user.getGender() == null){
 
-        response.sendRedirect("http://localhost:5173/users/addition-info");
+            response.sendRedirect("http://localhost:5173/users/addition-info");
+        }else{
+            response.sendRedirect("http://localhost:5173");
+        }
 
     }
 
