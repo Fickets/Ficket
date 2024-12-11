67557e6fd28fc21e0aeb63d04f08a4ed745aca7e
choiyongsu <cys980220@gmail.com>
2024-12-11 15:17:17 +0900

    [Feat] event statistic add

--- frontend/src/service/event/eventApi.ts
+++ frontend/src/service/event/eventApi.ts
@@ -1,6 +1,6 @@
 import { AxiosResponse } from "axios";
 import { privateApi, publicApi } from "../../utils/http-common";
-import { ResponseData, EventDetailRes } from "../../types/ApiResponseType";
+import { ResponseData, EventDetailRes, GenderStatisticRes } from "../../types/ApiResponseType";
 
 const url = "events"
 
@@ -9,6 +9,15 @@ export const eventDetail = async (
     Response: (Response: AxiosResponse<EventDetailRes>) => void,
     Error: (Error: AxiosResponse<ResponseData>) => void) => {
     await publicApi.get(`/${url}/detail/${eventId}`)
+        .then(Response)
+        .catch(Error)
+}
+
+export const genderStatistic = async (
+    eventId: number,
+    Response: (Response: AxiosResponse<ResponseData>) => void,
+    Error: (Error: AxiosResponse<ResponseData>) => void) => {
+    await publicApi.get(`/${url}/statistic/gender/${eventId}`)
         .then(Response)
         .catch(Error)
 }
