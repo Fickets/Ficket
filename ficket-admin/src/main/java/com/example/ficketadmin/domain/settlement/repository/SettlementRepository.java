9ace13181ef1157c95475d355dc275b8445a2644
choiyongsu <cys980220@gmail.com>
2024-12-27 19:06:24 +0900

    [FEAT] settlement refund add

--- ficket-admin/src/main/java/com/example/ficketadmin/domain/settlement/repository/SettlementRepository.java
+++ ficket-admin/src/main/java/com/example/ficketadmin/domain/settlement/repository/SettlementRepository.java
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SettlementRepository extends JpaRepository<Settlement, Long> {
+
+    Optional<Settlement> findByOrderId(Long orderId);
 }
