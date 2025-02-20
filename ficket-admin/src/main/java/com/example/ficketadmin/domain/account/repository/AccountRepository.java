608031776b055e88a440b5017e82a662d8f33961
오형상 <vyg1023@gmail.com>
2025-02-20 14:18:43 +0900

    [Fix] findByCompany 삭제

--- ficket-admin/src/main/java/com/example/ficketadmin/domain/account/repository/AccountRepository.java
+++ ficket-admin/src/main/java/com/example/ficketadmin/domain/account/repository/AccountRepository.java
@@ -1,13 +1,9 @@
 package com.example.ficketadmin.domain.account.repository;
 
 import com.example.ficketadmin.domain.account.entity.Account;
-import com.example.ficketadmin.domain.company.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
-    Optional<Account> findByCompany(Company company);
 
 }
