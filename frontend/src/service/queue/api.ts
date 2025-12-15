04c1db0bd29e95fb673089f8225145d9b7561621
오형상 <vyg1023@gmail.com>
2025-12-15 14:23:56 +0900

    [feat] 대기열 구조 변경

--- frontend/src/service/queue/api.ts
+++ frontend/src/service/queue/api.ts
@@ -1,42 +1,45 @@
 import { privateApi } from "../../utils/http-common.ts";
+import { MyQueueStatusResponse } from "../../types/queue.ts";
 
 // 대기열 진입 API
 export const enterQueue = async (eventId: string): Promise<void> => {
   try {
-    await privateApi.get<void>(`/queues/${eventId}/enter`);
+    await privateApi.post<void>(`/queues/${eventId}/enter-queue`);
   } catch (error) {
     console.error("대기열 진입 실패:", error);
     throw error;
   }
 };
 
-export const canEnterTicketingPage = async (
+export const leaveQueue = async (eventId: string): Promise<void> => {
+  try {
+    await privateApi.post<void>(`/queues/${eventId}/leave-queue`);
+  } catch (error) {
+    console.error("대기열 나가기 실패:", error);
+    throw error;
+  }
+};
+
+export const enterTicketing = async (eventId: string): Promise<boolean> => {
+  try {
+    const response = await privateApi.post(
+      `/queues/${eventId}/enter-ticketing`,
+    );
+    return response.data;
+  } catch (error) {
+    console.error(`티켓팅 진입 실패 ${eventId}:`, error);
+    return false; // 기본적으로 실패 시 false 반환
+  }
+};
+
+export const getQueueStatus = async (
   eventId: string,
-): Promise<boolean> => {
+): Promise<MyQueueStatusResponse> => {
   try {
-    const response = await privateApi.get(`/queues/${eventId}/can-enter`);
+    const response = await privateApi.get(`/queues/${eventId}/my-status`);
     return response.data;
   } catch (error) {
-    console.error(`이벤트에 대한 대기열 항목 확인 오류 ${eventId}:`, error);
-    return false; // 기본적으로 실패 시 false 반환
-  }
-};
-
-export const occupySlot = async (eventId: string): Promise<boolean> => {
-  try {
-    const response = await privateApi.post(`/queues/${eventId}/occupy-slot`);
-    return response.data;
-  } catch (error) {
-    console.error(`Error occupying slot for event ${eventId}:`, error);
-    return false; // 기본적으로 실패 시 false 반환
-  }
-};
-
-export const releaseSlot = async (eventId: string): Promise<void> => {
-  try {
-    await privateApi.delete(`/queues/${eventId}/release-slot`);
-  } catch (error) {
-    console.error(`Error occupying slot for event ${eventId}:`, error);
+    console.error(`나의 대기열 상태 조회 실패 ${eventId}:`, error);
     throw error;
   }
 };
--- frontend/src/types/queue.ts
+++ frontend/src/types/queue.ts
@@ -1,22 +1,5 @@
-export enum QueueStatus {
-  WAITING = "Waiting",
-  IN_PROGRESS = "In Progress",
-  ALMOST_DONE = "Almost Done",
-  COMPLETED = "Completed",
-  CANCELLED = "Cancelled",
-}
-
 export interface MyQueueStatusResponse {
-  userId: string;
-  eventId: string;
   myWaitingNumber: number;
   totalWaitingNumber: number;
-  queueStatus: QueueStatus;
-}
-
-export enum WorkStatus {
-  ORDER_RIGHT_LOST = "User lost the right to place an order",
-  SEAT_RESERVATION_RELEASED = "Seat reservation has been released",
-  ORDER_PAID = "Order Completed",
-  ORDER_FAILED = "Order Failed",
+  canEnter: boolean;
 }
