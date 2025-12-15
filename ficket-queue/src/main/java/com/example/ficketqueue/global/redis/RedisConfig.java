ebc5232b2f58b3904fc9916db2dc6c6a2a00e305
오형상 <vyg1023@gmail.com>
2025-12-15 13:35:37 +0900

    fix: redis Object json 직렬화 오류, @Configuration 누락 해결

--- ficket-queue/src/main/java/com/example/ficketqueue/global/redis/RedisConfig.java
+++ ficket-queue/src/main/java/com/example/ficketqueue/global/redis/RedisConfig.java
@@ -2,17 +2,20 @@ package com.example.ficketqueue.global.redis;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String host;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
     @Bean
@@ -20,25 +23,23 @@ public class RedisConfig {
         return new LettuceConnectionFactory(host, port);
     }
 
-
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory connectionFactory) {
+
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
-        // Key는 String으로 직렬화
+        // Key는 항상 String
         template.setKeySerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
 
-        // Value도 String으로 직렬화 (숫자도 문자열로 저장)
-        template.setValueSerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
+        // Value는 JSON
+        GenericJackson2JsonRedisSerializer jsonSerializer =
+                new GenericJackson2JsonRedisSerializer();
 
-        // 모든 경우
-        template.setDefaultSerializer(new StringRedisSerializer());
-
-        // Lua 스크립트 실행을 위해 필수
-        template.setEnableTransactionSupport(false);
+        template.setValueSerializer(jsonSerializer);
+        template.setHashValueSerializer(jsonSerializer);
 
         template.afterPropertiesSet();
         return template;
